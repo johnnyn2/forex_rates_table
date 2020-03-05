f897d2ada7c865191a52d8e7a938c11e09ae5aee
wai kin ho <wkhoae@connect.ust.hk>
2020-03-05 19:14:01 +0800

    create table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+
+const useApi = (initialData) => {
+  const [data, setData] = useState(initialData);
+  const url = 'http://data.fixer.io/api/latest?access_key=fe3a9b3f9ac173a3e51d8ea22951cf95';
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await fetch(url);
+        const data = await result.json();
+        console.log("json data: ", data);
+        setData(data);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return {data, isLoading, isError};
+}
 
 function App() {
+  const {data, isLoading, isError} = useApi();
+  const rates = data ? data.rates : {};
+  const head = (
+    <thead>
+      <tr>
+        <th style={{padding: 15}}>
+          Key
+        </th>
+        <th style={{padding: 15}}>
+          Original Currency
+        </th>
+        <th style={{padding: 15}}>
+          Currency added by 10.002
+        </th>
+      </tr>
+    </thead>
+  );
+  const rows = isLoading || isError ? 
+    (
+      <tbody>
+        <tr>
+          <td style={{padding: 15}}>{isLoading ? 'Loading...' : 'API Error...'}</td>
+          <td style={{padding: 15}}>{isLoading ? 'Loading...' : 'API Error...'}</td>
+          <td style={{padding: 15}}>{isLoading ? 'Loading...' : 'API Error...'}</td>
+        </tr>
+      </tbody>
+    ) : Object.keys(rates).map(key => {
+    const temp = rates[key] + 10.0002;
+    return (
+      <tbody key={key}>
+        <tr>
+          <td style={{padding: 15}}>
+            {key}
+          </td>
+          <td
+            style={{
+              padding: 15,
+              border: rates[key] % 2 === 0 || key === 'HKD' ?
+                '5px solid red' : ''
+            }}
+          >
+            {rates[key]}
+          </td>
+          <td
+            style={{
+              padding: 15,
+              border: rates[key] % 2.0 === 0 || key === 'HKD' ?
+                '5px solid red' : ''
+            }}
+          >
+            {temp}
+          </td>
+        </tr>
+      </tbody>
+    );
+  });
+  const table = (
+    <table>
+      {head}
+      {rows}
+    </table>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      {table}
     </div>
   );
 }
